55f19a56e9ffdd36aac95cefbbc2346bacefe438
King Pra <kingi.pra@gmail.com>
2019-05-09 19:43:53 -0500

    oh the joy

--- src/richieRich/RichieRich.java
+++ src/richieRich/RichieRich.java
@@ -4,6 +4,7 @@ public class RichieRich {
 	public static void main(String[] arg) {
 		System.out.println("this is a test");
 		System.out.println("King, I want you to see this!");
+		System.out.println("Rich, oh joy it works!");
 	}
 
 }
